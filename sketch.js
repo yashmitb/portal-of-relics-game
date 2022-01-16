d359282e5425b49b1e4e00b5b76d13f8b711a50f
yashmitb <yashmitb07@gmail.com>
2022-01-16 11:52:40 -0800

    player movement, grass gen, world borders

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ let ocean = "#008dc4";
 let shore = "#00a9cc";
 let sand = "#eecda3";
 let grass = "#7ec850";
+let darkGrass = "#6dad45";
 let stone = "#676767";
 let snow = "#fffafa";
 let k = 0, t = 0, m = 0;
@@ -11,6 +12,8 @@ let k = 0, t = 0, m = 0;
 var gameState = 'form';
 
 var form;
+
+var grassTileCreated = 0;
 
 var left_key = false;
 var up_key = false;
@@ -25,6 +28,10 @@ var boxSpeedY = 0;
 var box;
 var bg;
 var bo1;
+var grassTileGroup;
+var random123 = 0;
+
+var edge1, edge2, edge3, edge4;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -39,13 +46,18 @@ function setup() {
 
   drawMap();
 
+  grassTileGroup = new Group();
   form = new Form();
   form.display();
   fill("gray");
-  box = createSprite(windowWidth/2,windowHeight/2,windowWidth/2- 1000,windowWidth/2- 1000);
-  bo1 = createSprite(300,300,300,300);
+  box = createSprite(windowWidth / 2, windowHeight / 2, windowWidth / 2 - 1000, windowWidth / 2 - 1000);
+  box.depth = 10000000;
+  
   box.visible = false;
+  box.shapeColor = color(255, 0, 0);
   bg = loadImage('assets/bg.jpg');
+
+  fill("red");
 
 }
 
@@ -55,50 +67,84 @@ function draw() {
   }
   if (gameState === 'play') {
     // console.log("asd");
-    
+
     background(200);
     movebox();
-    fill('red');
     box.visible = true;
     box.position.x = boxX;
     box.position.y = boxY;
     camera.position.x = box.position.x;
     camera.position.y = boxY;
+    
+    if(grassTileCreated === 0){
+    for(var u=0; u<40;u++){
+      for(var i=0;i<40;i++){
+        createGrassTile(windowWidth - i*100,random123);
+      }
+      random123 = random123+100;
+    }
+    grassTileCreated = 1;
 
   }
 
 
-  console.log(boxX);
+  }
+
+  console.log(box.position);
+  // console.log(boxY);
+  // console.log(-windowHeight * 2);
 
   drawSprites();
 }
+
+function createGrassTile(x,y){
+  bo1 = createSprite(x,y, 100, 100);
+  console.log(Math.random(0,1))
+  var randomNum = Math.round(random(0,1));
+  if(randomNum === 1){
+    bo1.shapeColor = color(darkGrass);
+  }
+  if(randomNum === 0){
+    bo1.shapeColor = color(grass);
+  }
+  bo1.depth = 0;
+  grassTileGroup.add(bo1);
+}
+
+function createGrassTileRect(x,y){
+  var randomNum = Math.round(random(0,1));
+  if(randomNum === 1){
+    fill("green");
+  }
+  if(randomNum === 0){
+    fill("darkgreen");
+  }
+  rect(x,y, 40, 40);
+  // grassTileGroup.add(bo1);
+}
+
 
 function movebox() {
   boxX += boxSpeedX;
   boxY += boxSpeedY;
   boxSpeedX *= 0.95;
   boxSpeedY *= 0.95;
-  if (left_key) {
+  
+  if (left_key&&box.position.x>-1890) {
     boxSpeedX -= 0.3
   }
-  if (right_key) {
+  if (right_key && box.position.x< 1830) {
     boxSpeedX += 0.3
   }
-  if (up_key) {
+  if (up_key && box.position.y>90) {
     boxSpeedY -= 0.3
   }
-  if (down_key) {
+  if (down_key && box.position.y<3810) {
     boxSpeedY += 0.3
   }
 
-  //EDGES 
-  if (boxX > width || boxX < 0) {
-    // boxSpeedX *= -1
-  }
-  if (boxY > height || boxY < 0) {
-    // boxSpeedY *= -1
-  }
-  //TRACK CENTRE
+
+
 }
 
 function keyPressed() {
